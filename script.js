79705d47c44969568a66d6b1ca4551baca7a1b33
viiiii36 <vi2015nguyen2015@gmail.com>
2024-03-04 23:28:11 -0800

    almost done

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 //Player 1 is X, player 2 is O
 playGame=()=>{
     let i = 1;
+    //Use array to decide who wins
+    let array1=[];
+    let array2=[];
 $('h3').text(`It's player 1 turn`);
 $('.play').each(function(){
     $(this).click(function(){
@@ -11,40 +14,88 @@ $('.play').each(function(){
             //Player 2 turn, modifying the div into "O" string and modify the header 3 to reflect next turn 
             if (i%2==0){
                 $(`td#${id}`).text('O');
+                array2.push(id);
+                //sort the player's array in ascending order to compare with the winning array
+                $.each(array2, function(index,value) {
+                    array2.sort(function(a, b){return a-b});})
+                //call function to see if this player wins this turn and pop a message
+                if(winning(array2)=='win'){
+                    $('#win').html(`<h4 class="alert-heading">Game Over</h4>
+                    <p>Player 2 won!</p>`)
+                };
                 $('h3').text(`It's player 1 turn`);
+                
             }
             //Player 1 turn, modifying the div into "X" string and modify the header 3 to reflect next turn 
             else{
                 $(`td#${id}`).text('X');
+                array1.push(id);
+                //sort the player's array in ascending order to compare with the winning array
+                $.each(array1, function(index,value) {
+                    array1.sort(function(a, b){return a-b});})
+                    if(winning(array1)=='win'){
+                        $('#win').html(`<h4 class="alert-heading">Game Over</h4>
+                        <p>Player 1 won!</p>`)
+                    };
                 $('h3').text(`It's player 2 turn`);}
+            console.log(array1);
+            console.log(array2);
             console.log(i);
             i++;
         }
         else{
             
         }
+        
     })
 })
 }
 //function playGame is going to be add into reset button so we can play another game
 playGame();
 
-/*
-Orignal array [0,1,2; 3,4,5; 6,7,8]
-Winning combination:
-Horizontal: [0,1,2] [3,4,5] [6,7,8]
-Vertical: [0,3,6] [1,4,7] [2,5,8]
-Diagonal: [0,4,8] [2,4,6]
-*/
+//Making a restart button, which is going to set all the div element back to the original buttons 
 $( "#restart" ).on( "click", function() {
+    $('#win').text('');
     $('h3').text(`It's player 1 turn`);
     i=1;
-    for (k=0;k<9;k++){
+    for (k=0;k<9;k++){ 
         $(`td#${k}`).html(`<button id="${k}" type="button" class="btn btn-info play">Check</button>`)
     }
+    //and call playGame function to start a new game
     playGame();
     console.log(i);
   });
+  /*
+Winning combination:
+HOrizontal: [0,1,2] [3,4,5] [6,7,8]
+Vertical: [0,3,6] [1,4,7] [2,5,8]
+Diagonal: [0,4,8] [2,4,6]
+*/
+
+let winningArray=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+//function to compare the player's array vs the winning array to see if they win or not
+winning=(arr)=>{
+    //only when there are more than 3 x's or o's to be able to win, we dont need to worry when player's array is less than 3
+    if (arr.length>2){
+            //compare player's array to each winning array
+            for (j=0;j<winningArray.length;j++){
+                let winAr=[];
+                for(n=0;n<3;n++){
+                    var found = $.inArray(winningArray[j][n], arr);
+                    if( !(-1 == found) ) {
+                        winAr.push(winningArray[j][n]);
+                    }
+                if (winAr.length=3){
+                    return 'win';
+                    break;
+                }
+                }
+                }
+            }
+    };
+          
+    
+
 
 
     
